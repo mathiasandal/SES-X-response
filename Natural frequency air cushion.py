020e9861fff00469e6ab654bff5da72fdc215e78
mathiasandal <mathias.andal@gmail.com>
2022-01-27 15:52:18 +0100

    Define total mass of the vessel as the sum of the hydrostatic force at the given draught and the air cushion lift.

--- Natural frequency air cushion.py
+++ Natural frequency air cushion.py
@@ -1,10 +1,10 @@
 import numpy as np
-from air_cushion import air_cushion_area, interpolate_fan_characteristics, read_fan_characteristics
+from air_cushion import interpolate_fan_characteristics, read_fan_characteristics
 
 # Main parameters of conceptual SES with rectangular side hulls
 L = 20  # [m] length of the vessel
 B = 10  # [m] total beam of the vessel
-b_G = 1.5  # [m] beam of side hulls (gondolas)
+b_G = 0.5  # [m] beam of side hulls (gondolas)
 b_c = 7  # [m] beam of the air cushion/beam between the side hulls
 h_hull = 2  # [m] distance from bottom of side hull to roof of air cushion
 d = 0.5  # [m] draught without excess pressure in the air cushion
@@ -17,15 +17,14 @@ p_a = 101325  # [Pa] atmospheric pressure
 g = 9.81  # [m/s^2] acceleration of gravity
 
 
-volume_displacement = 2 * L * d * b_G  # [m^3] volume displacement without excess air cushion pressure
-M = volume_displacement * rho  # [kg] total mass of the vessel
-
 h = p_0/rho/g  # [m] Difference in water level inside and outside the air cushion
 
 h_b = h_hull + h - d  # [m] height of the air cushion after the water level inside the cushion i suppressed.
 
 A_b = L * b_c  # [m] Area of the air cushion
 
+volume_displacement = 2 * L * d * b_G  # [m^3] volume displacement without excess air cushion pressure
+M = volume_displacement * rho + p_0*A_b/g  # [kg] total mass of the vessel
 
 # Read in fan characteristics
 Q, P, rpm_dummy = read_fan_characteristics('Input files/fan characteristics/fan characteristics.csv', '1800rpm')
@@ -54,5 +53,6 @@ zeta = c/c_cr  # [-] Damping ratio of the system
 
 print('Natural frequency:\t', round(omega_0, 2), '\t[rad/s]')
 print('Natural frequency:\t', round(omega_0/2/np.pi, 2), '\t[Hz]')
+print('Natural period: \t', round(2*np.pi/omega_0, 2), '\t[s]')
 print('Damping ratio:\t\t', round(zeta, 3), '\t[-]')
 
