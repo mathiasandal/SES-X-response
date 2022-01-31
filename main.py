e4465a1da997e0b3b69643fa71ddb3d219b689fa
mathiasandal <mathias.andal@gmail.com>
2022-01-31 17:39:18 +0100

    Apply the read_re7_file(...) for an input with and air cushion for a conceptual SES

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-# This is a sample Python script.
+from veres import read_re7_file
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+path_re7 = 'Input files/Conceptual SES/with air cushion/input.re7'
 
-import numpy as np
-import matplotlib.pyplot as plt
+VMAS, ADDMAS, DAMP, REST, VEL, HEAD, FREQ, XMTN, ZMTN, NDOF = read_re7_file(path_re7)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(9)
