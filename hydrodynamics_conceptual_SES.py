2ee7eed0f06759b891e05eefa1d5008bbddc4ee8
mathiasandal <mathias.andal@gmail.com>
2022-02-01 18:33:00 +0100

    Added Restoring coefficients from bow and stern seals

--- hydrodynamics_conceptual_SES.py
+++ hydrodynamics_conceptual_SES.py
@@ -1,5 +1,6 @@
 from air_cushion import read_fan_characteristics, interpolate_fan_characteristics
 from veres import read_re7_file
+from bow_and_stern_seals import restoring_finger_at_bow_lobe_bag_at_the_stern
 
 """
 This script compares calculations from Veres on a simplified SES geometry with rectangular side hulls. 
@@ -24,16 +25,14 @@ p_a = 101325  # [Pa] atmospheric pressure
 g = 9.81  # [m/s^2] acceleration of gravity
 gamma = 1.4  # [-] Ratio of specific heat for air, when assuming adiabatic density relation
 
-
 # Some derived parameters
 A_wp = 2 * b_G * L  # [m^2] water plane area of the side hulls
-I_x = 2 * (b_G**3 * L / 12 + (b_c/2 + b_G/2)**2 * b_G * L)  # [m^4] second moment of area of A_wp about x-axis.
-I_y = 2 * (L**3 * b_G / 12)  # [m^4] second moment of area of A_wp about y-axis
+I_x = 2 * (b_G ** 3 * L / 12 + (b_c / 2 + b_G / 2) ** 2 * b_G * L)  # [m^4] second moment of area of A_wp about x-axis.
+I_y = 2 * (L ** 3 * b_G / 12)  # [m^4] second moment of area of A_wp about y-axis
 V_0 = A_wp * d  # [m^3] volume displaced by the side hulls at equilibrium
 
-
 # ------- Air cushion properties -------
-h = p_0/rho/g  # [m] Difference in water level inside and outside the air cushion
+h = p_0 / rho / g  # [m] Difference in water level inside and outside the air cushion
 h_b1 = h_hull + h - d  # [m] height of the air cushion after the water level inside the cushion i suppressed.
 h_b = h_hull - d  # [m] height of the air cushion before the water level inside the cushion i suppressed.
 A_b = L * b_c  # [m] Area of the air cushion
@@ -51,7 +50,7 @@ M = V_0 * rho + p_0 * A_b / g  # [kg] total mass of the vessel
 
 # ------- Coordinate systems -------
 # Motion coordinate system located relative to the intersection of AP, CL and BL.
-x_m = L/2  # [m] longitudinal position
+x_m = L / 2  # [m] longitudinal position
 y_m = 0  # [m] transverse position
 z_m = d  # [m] vertical position
 
@@ -63,12 +62,27 @@ z_G = 0  # [m] vertical position of the center of gravity relative to the motion
 # Centroid of the air cushion at equilibrium relative to the motion coord. system
 x_c = 0  # [m] longitudinal position
 y_c = 0  # [m] transverse position
-z_c = -h/2  # [m] vertical position
+z_c = -h / 2  # [m] vertical position
 
 # Centroid of the submerged hull volume relative to the motion coord. system
 x_B = 0  # [m] longitudinal position
 y_B = 0  # [m] transverse position
-z_B = -d/2  # [m] vertical position
+z_B = -d / 2  # [m] vertical position
+
+# ------- Bow and stern seals ------
+# The vessel has a finger seal at the bow and a lobe bag type of seal at the back
+
+# properties
+b_seals = b_c  # [m] Beam of the seals
+tau_b = 60  # [deg] angle of the bow finger seal
+tau_s = 30  # [deg] angle of the stern lobe bag seal
+p_s = 0  # [Pa] Membrane seal pressure
+x_lobe_bag_seal = -L / 2  # [m] Longitudinal position of the lobe bag seal relative to motion coord. system
+x_finger_seal = L / 2  # [m] Longitudinal position of the finger seal relative to motion coord. system
+
+# Computes restoring coefficients
+c_33_seal, c_35_seal, c_55_seal = restoring_finger_at_bow_lobe_bag_at_the_stern(b_seals, tau_b, tau_s, p_0, p_s,
+                                                                                x_finger_seal, x_lobe_bag_seal)
 
 # ------- Hydrodynamic coefficients -------
 
@@ -91,7 +105,6 @@ C_55 = C_55_h + C_55_c  # [Nm] total restoring coefficient
 B_73_c = A_b  # [m] equivalent damping coefficient in uniform pressure DOF due to velocity in heave.
 B_75_c = -A_b * x_B  # [m^2] equivalent damping coefficient in uniform pressure DOF due to velocity in pitch.
 B_77_c = p_0 * V_c0 / gamma / (p_0 + p_a)  # [m^3] equivalent damping coefficient in uniform pressure DOF.
-
 
 # ------- Comparison with Veres ------
 
