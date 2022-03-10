92f414e0d502b57705180fdcfa4042a47c294f6f
mathiasandal <mathias.andal@gmail.com>
2022-03-10 14:44:12 +0100

    Plotted RAOs with wavepumping that have the same amplitude for all amplitudes.

--- compute_RAO_BBGreen.py
+++ compute_RAO_BBGreen.py
@@ -1,6 +1,6 @@
 from veres import read_re7_file, read_re8_file
 from air_cushion import wave_pumping_excitation_sesx, air_cushion_area, read_fan_characteristics, \
-    interpolate_fan_characteristics
+    interpolate_fan_characteristics, wave_pumping_rect
 from mass_matrix import create_mass_matrix
 from Wave_response_utilities import add_row_and_column
 import numpy as np
@@ -31,7 +31,7 @@ l_1 = 12   # 0.0001  #     [m] length of the rectangular part of the air cushion
 l_2 = 6  # 0.001  #      [m] length of the triangular part of the air cushion
 b = 3.4  # [m] beam of the air cushion
 p_0 = 3500  # [Pa] mean excess pressure in the air cushion
-h_b = 0.64  # [m]  Height of the air cushion  # TODO: make sure this value is correct
+h_b = 0.64  #2 # [m]  Height of the air cushion  # TODO: make sure this value is correct
 h = p_0 / rho / g  # [m] Difference in water level inside and outside the air cushion
 
 # Compute area and longitudinal position of centroid relative to AP
@@ -75,18 +75,46 @@ omega_e = omega_0 + U / g * np.multiply(np.power(omega_0, 2), np.cos(np.rad2deg(
 n_frequencies = len(FREQ_re7)  # number of wave frequencies
 
 # Initialize numpy array to contain complex excitation amplitudes
-f_ex = np.zeros([6, n_frequencies], dtype=complex)
+f_ex_1 = np.zeros([6, n_frequencies], dtype=complex)
 
 # fill the array with Froude Krylov and diffraction loads
 for i in range(n_frequencies):
     for j in range(6):
-        f_ex[j, i] = REFORCE[j, i, 0, 0] + 1j * IMFORCE[j, i, 0, 0]
+        f_ex_1[j, i] = REFORCE[j, i, 0, 0] + 1j * IMFORCE[j, i, 0, 0]
 
 # compute wave pumping
-f_7 = wave_pumping_excitation_sesx(x_f, x_s, y_s, x_b, omega_e, beta)
+f_7_test = 1j*wave_pumping_rect(x_b, x_s, y_p, y_s, omega_e, beta)
+#f_7_test = np.ones([n_frequencies])*30
+f_7_wp = wave_pumping_excitation_sesx(x_f, x_s, y_s, x_b, omega_e, beta)
 
-f_ex = np.vstack([f_ex, f_7])  # append the wave pumping as a seventh excitation
+f_7 = f_7_wp
 
+# plot wavepumping
+encounter_frequency_Hz = omega_e / 2 / np.pi  # compute encounter frequency in Hz
+k_encounter = np.power(omega_e, 2) / g  # encounter wave number
+encounter_wavelength = np.divide(2*np.pi, k_encounter)
+
+
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.plot(encounter_frequency_Hz, np.abs(f_7_wp), label='SES-X shape')
+ax1.plot(encounter_frequency_Hz, np.abs(f_7_test), label='Test')
+fig.suptitle(
+    'Wave pumping excitation for SES-X air cushion. $l_1 = %.0f$m, ' % l_1 + '$l_2 = %.0f$m, ' % l_2 + '$b = %0.1f$m, ' % b + '$\\beta = %0.1f^{\circ}$' % beta)
+ax1.set_xlabel("encounter frequency [Hz]")
+ax1.set_ylabel('$F_{wp} [m^3/s]$')
+ax1.set_xlim([0, 12])
+ax1.legend()
+ax2.plot(encounter_frequency_Hz, np.angle(f_7_wp), label='SES-X shape')
+ax2.plot(encounter_frequency_Hz, np.angle(f_7_test), label='Test')
+ax2.set_xlabel("encounter frequency [Hz]")
+ax2.set_ylabel('Phase shift')
+ax2.set_xlim([0, 12])
+ax2.legend()
+plt.show()
+
+
+f_ex = np.vstack([f_ex_1, f_7])  # append the wave pumping as a seventh excitation
+f_ex_test = np.vstack([f_ex_1, f_7_test])
 # TODO: Notice signs of these values. Should be fixed in content from project thesis
 x_G = x_prime - lcg  # [m] longitudinal position of COG relative to the motion coordinate system
 z_G = -(z_prime - vcg)  # [m] vertical position of COG relative to the motion coordinate system
@@ -140,40 +168,109 @@ B[:, 6, 2] += B_73_c
 B[:, 6, 4] += B_75_c
 B[:, 6, 6] += B_77_c
 
+# Correct stiffness matrix for change of center of gravity
+vcg_veres = 0.5  # [m] vertical center of gravity used in Veres  # TODO: Get this automatically
+C[:, 3, 3] -= total_mass * g * (vcg_veres - vcg)
+C[:, 4, 4] -= total_mass * g * (vcg_veres - vcg)
+
 # Compute RAOs
 
 # initialize array to contain complex force amplitudes
 raos = np.zeros([7, n_frequencies], dtype=complex)
+raos_test = np.zeros([7, n_frequencies], dtype=complex)
 
 # Compute and store complex response amplitudes
 
 for i in range(n_frequencies):
     raos[:, i] = la.solve(-omega_e[i]**2 * (M + A[i]) + 1j * omega_e[i] * B[i] + C[i], f_ex[:, i])
+    raos_test[:, i] = la.solve(-omega_e[i] ** 2 * (M + A[i]) + 1j * omega_e[i] * B[i] + C[i], f_ex_test[:, i])
 
 raos_magnitude = np.transpose(np.abs(raos))  # store the magnitude of the complex response amplitude
 raos_phase = np.transpose(np.rad2deg(np.angle(raos)))  # store the phase of the complex response amplitude
+raos_magnitude_test = np.transpose(np.abs(raos_test))  # store the magnitude of the complex response amplitude
+raos_phase_test = np.transpose(np.rad2deg(np.angle(raos_test)))  # store the phase of the complex response amplitude
 
 
 # Plot RAOs
 
 plot_raos = True
 
-max_encounter_frequency = 1000  # Hz
+max_encounter_frequency = 20  # Hz
 
-encounter_frequency_Hz = omega_e / 2 / np.pi  # compute encounter frequency in Hz
 
 if plot_raos:
-    plt.plot(encounter_frequency_Hz, raos_magnitude[:, 2], '-x', label='python')
+    plt.plot(encounter_frequency_Hz, raos_magnitude[:, 2], '-x', label='SES-X shape')
+    plt.plot(encounter_frequency_Hz, raos_magnitude_test[:, 2], '-x', label='Test')
     plt.xlabel('encounter frequency [Hz]')
     plt.ylabel('$\\eta_{3}/\\zeta_a$')
-    plt.title('RAO in heave')
+    plt.title('RAO in heave at 22kn and head sea')
     plt.xlim([0, max_encounter_frequency])
+    plt.legend()
     plt.show()
 
-    plt.plot(encounter_frequency_Hz, np.multiply(np.power(omega_e, 2), raos_magnitude[:, 2]), '-x', label='python')
+    plt.plot(encounter_frequency_Hz, np.multiply(np.power(omega_e, 2), raos_magnitude[:, 2]), '-x', label='SES-X shape')
+    plt.plot(encounter_frequency_Hz, np.multiply(np.power(omega_e, 2), raos_magnitude_test[:, 2]), '-x', label='Test')
     plt.xlabel('encounter frequency [Hz]')
     plt.ylabel('$|\\omega^2\\eta_{3}|/\\zeta_a[s^{-2}]$')
     plt.title('RAO for acceleration in heave')
     plt.xlim([0, max_encounter_frequency])
-    plt.ylim([0, np.max(np.multiply(np.power(omega_e, 2), raos_magnitude[:, 2])[encounter_frequency_Hz < max_encounter_frequency])])
+    plt.ylim([0, np.max(np.multiply(np.power(omega_e, 2), raos_magnitude_test[:, 2])[encounter_frequency_Hz < max_encounter_frequency])])
+    plt.legend()
+    plt.show()
+
+    plt.plot(encounter_frequency_Hz, raos_magnitude[:, 6], fillstyle='none', marker='o', linestyle='-', label='SES-X shape')
+    plt.plot(encounter_frequency_Hz, raos_magnitude_test[:, 6], fillstyle='none', marker='o', linestyle='-', label='Test')
+    plt.xlabel('encounter frequency [Hz]')
+    plt.title('RAO in uniform pressure at 22kn and head sea')
+    plt.ylabel('$\\eta_{7} [-]$')
+    plt.xlim([0, max_encounter_frequency])
+    plt.legend()
+    plt.show()
+
+    plt.plot(encounter_frequency_Hz, np.divide(raos_magnitude[:, 4], k_encounter), '-x', label='SES-X shape')
+    plt.plot(encounter_frequency_Hz, np.divide(raos_magnitude_test[:, 4], k_encounter), '-x', label='Test')
+    plt.xlabel('encounter frequency [Hz]')
+    plt.ylabel('$\\eta_{5}/k\\zeta_a$')
+    plt.title('RAO in pitch at 22kn and head sea')
+    plt.xlim([0, max_encounter_frequency])
+    plt.legend()
+    plt.show()
+
+    plt.plot(encounter_wavelength, raos_magnitude[:, 2], '-x', label='SES-X shape')
+    plt.plot(encounter_wavelength, raos_magnitude_test[:, 2], '-x', label='Test')
+    plt.xlabel('encounter wavelength [m]')
+    plt.ylabel('$\\eta_{3}/\\zeta_a$')
+    plt.title('RAO in heave at 22kn and head sea')
+    plt.xlim([0, max_encounter_frequency])
+    plt.legend()
+    plt.show()
+
+    plt.plot(encounter_wavelength, np.multiply(np.power(omega_e, 2), raos_magnitude[:, 2]), '-x', label='python')
+    plt.plot(encounter_wavelength, np.multiply(np.power(omega_e, 2), raos_magnitude_test[:, 2]), '-x', label='Test')
+    plt.xlabel('encounter wavelength [m]')
+    plt.ylabel('$|\\omega^2\\eta_{3}|/\\zeta_a[s^{-2}]$')
+    plt.title('RAO for acceleration in heave')
+    plt.xlim([0, max_encounter_frequency])
+    plt.ylim([0, np.max(
+        np.multiply(np.power(omega_e, 2), raos_magnitude[:, 2])[encounter_frequency_Hz < max_encounter_frequency])])
+    plt.legend()
+    plt.show()
+
+    plt.plot(encounter_wavelength, raos_magnitude[:, 6], fillstyle='none', marker='o', linestyle='-',
+             label='SES-X shape')
+    plt.plot(encounter_wavelength, raos_magnitude_test[:, 6], fillstyle='none', marker='o', linestyle='-',
+             label='Test')
+    plt.xlabel('encounter wavelength [m]')
+    plt.title('RAO in uniform pressure at 22kn and head sea')
+    plt.ylabel('$\\eta_{7} [-]$')
+    plt.xlim([0, max_encounter_frequency])
+    plt.legend()
+    plt.show()
+
+    plt.plot(encounter_wavelength, np.divide(raos_magnitude[:, 4], k_encounter), '-x', label='SES-X shape')
+    plt.plot(encounter_wavelength, np.divide(raos_magnitude_test[:, 4], k_encounter), '-x', label='Test')
+    plt.xlabel('encounter wavelength [m]')
+    plt.ylabel('$\\eta_{5}/k\\zeta_a$')
+    plt.title('RAO in pitch at 22kn and head sea')
+    plt.xlim([0, max_encounter_frequency])
     plt.show()
