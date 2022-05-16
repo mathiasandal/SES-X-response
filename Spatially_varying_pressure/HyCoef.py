474fa0526b75ee833aeca569f6f388ff4a02834a
mathiasandal <mathias.andal@gmail.com>
2022-05-16 18:00:55 +0200

    Made an if-statement determining if end terms are included or nor

--- Spatially_varying_pressure/HyCoef.py
+++ Spatially_varying_pressure/HyCoef.py
@@ -16,35 +16,47 @@ def compute_hydrodynamic_coeff(L_c, Bs, U, omega_0, rho_w=1025., g=9.81):
     # 2D damping term of a rectangular cross-section
     B_33_2D = 2 * rho_w * Bs**2 * omega_e * (1 - np.tanh(4*omega_e**np.sqrt(Bs/2/g) - np.pi))
 
-    # Expressions without the end terms are commented out
-    A_33_0 = A_33_2D * L_c
-    A_33 = A_33_0
-    #A_33 = A_33_0 - np.divide(U*B_33_2D, np.power(omega_e, 2))
+    include_end_terms = True
+    if include_end_terms:
+        A_33_0 = A_33_2D * L_c
+        A_33 = A_33_0 - np.divide(U * B_33_2D, np.power(omega_e, 2))
 
-    B_33_0 = B_33_2D * L_c
-    B_33 = B_33_0
-    #B_33 = B_33_0 + U * A_33_2D
+        B_33_0 = B_33_2D * L_c
+        B_33 = B_33_0 + U * A_33_2D
 
-    A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2))
-    #A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2)) - \
-    #       np.divide(U**2, A_33_2D, np.power(omega_e, 2))
+        A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2)) - \
+               np.divide(U**2, A_33_2D, np.power(omega_e, 2))
 
-    B_35 = U * A_33_0
-    #B_35 = U * A_33_0 + U*L_c*A_33_2D/2. - np.divide(U**2*B_33_2D, np.power(omega_e, 2))
+        B_35 = U * A_33_0 + U*L_c*A_33_2D/2. - np.divide(U**2*B_33_2D, np.power(omega_e, 2))
 
-    A_53 = np.divide(U * B_33_0, np.power(omega_e, 2))
-    #A_53 = np.divide(U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2))
+        A_53 = np.divide(U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2))
 
-    B_53 = -U * A_33_0
-    #B_53 = -U * A_33_0 + U*L_c*A_33_2D/2.
+        B_53 = -U * A_33_0 + U*L_c*A_33_2D/2.
 
-    A_55 = A_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * A_33_0, np.power(omega_e, 2))
-    #A_55 = A_33_2D * L_c**3/12. + np.divide(U**2 * A_33_0, np.power(omega_e, 2)) - \
-    #       np.divide(U*L_c**2*B_33_2D, np.power(2*omega_e, 2)) + np.divide(U**2*L_c*A_33_2D, 2*np.power(omega_e, 2))
+        A_55 = A_33_2D * L_c**3/12. + np.divide(U**2 * A_33_0, np.power(omega_e, 2)) - \
+               np.divide(U*L_c**2*B_33_2D, np.power(2*omega_e, 2)) + np.divide(U**2*L_c*A_33_2D, 2*np.power(omega_e, 2))
 
-    B_55 = B_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * B_33_0, np.power(omega_e, 2))
-    #B_55 = B_33_2D*L_c**3/12. + np.divide(U**2 * B_33_0, np.power(omega_e, 2)) + U*L_c**2*A_33_2D/4. \
-    #       - np.divide(U**2 * L_c * B_33_2D, 2*np.multiply(omega_e, 2))
+        B_55 = B_33_2D*L_c**3/12. + np.divide(U**2 * B_33_0, np.power(omega_e, 2)) + U*L_c**2*A_33_2D/4. \
+               - np.divide(U**2 * L_c * B_33_2D, 2*np.multiply(omega_e, 2))
+
+    else:
+        A_33_0 = A_33_2D * L_c
+        A_33 = A_33_0
+
+        B_33_0 = B_33_2D * L_c
+        B_33 = B_33_0
+
+        A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2))
+
+        B_35 = U * A_33_0
+
+        A_53 = np.divide(U * B_33_0, np.power(omega_e, 2))
+
+        B_53 = -U * A_33_0
+
+        A_55 = A_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * A_33_0, np.power(omega_e, 2))
+
+        B_55 = B_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * B_33_0, np.power(omega_e, 2))
 
     # TODO: Make sure if I need to multiply by 2 to include both side hulls
     return 2*A_33, 2*A_35, 2*A_53, 2*A_55, 2*B_33, 2*B_35, 2*B_53, 2*B_55
