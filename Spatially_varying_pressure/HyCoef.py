eb19215c847966a4067dc687599847b27e0e6dad
mathiasandal <mathias.andal@gmail.com>
2022-05-16 15:30:47 +0200

    Perform analysis with hydrodynamic coefficients without end terms.

--- Spatially_varying_pressure/HyCoef.py
+++ Spatially_varying_pressure/HyCoef.py
@@ -18,44 +18,44 @@ def compute_hydrodynamic_coeff(L_c, Bs, U, omega_0, rho_w=1025., g=9.81):
 
     # Expressions without the end terms are commented out
     A_33_0 = A_33_2D * L_c
-    # A_33 = A_33_0
-    A_33 = A_33_0 - np.divide(U*B_33_2D, np.power(omega_e, 2))
+    A_33 = A_33_0
+    #A_33 = A_33_0 - np.divide(U*B_33_2D, np.power(omega_e, 2))
 
     B_33_0 = B_33_2D * L_c
-    #B_33 = B_33_0
-    B_33 = B_33_0 + U * A_33_2D
+    B_33 = B_33_0
+    #B_33 = B_33_0 + U * A_33_2D
 
-    #A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2))
-    A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2)) - \
-           np.divide(U**2, A_33_2D, np.power(omega_e, 2))
+    A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2))
+    #A_35 = np.divide(-U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2)) - \
+    #       np.divide(U**2, A_33_2D, np.power(omega_e, 2))
 
-    #B_35 = U * A_33_0
-    B_35 = U * A_33_0 + U*L_c*A_33_2D/2. - np.divide(U**2*B_33_2D, np.power(omega_e, 2))
+    B_35 = U * A_33_0
+    #B_35 = U * A_33_0 + U*L_c*A_33_2D/2. - np.divide(U**2*B_33_2D, np.power(omega_e, 2))
 
-    #A_53 = np.divide(U * B_33_0, np.power(omega_e, 2))
-    A_53 = np.divide(U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2))
+    A_53 = np.divide(U * B_33_0, np.power(omega_e, 2))
+    #A_53 = np.divide(U * B_33_0, np.power(omega_e, 2)) - np.divide(U*L_c*B_33_2D, 2*np.power(omega_e, 2))
 
-    #B_53 = -U * A_33_0
-    B_53 = -U * A_33_0 + U*L_c*A_33_2D/2.
+    B_53 = -U * A_33_0
+    #B_53 = -U * A_33_0 + U*L_c*A_33_2D/2.
 
-    #A_55 = A_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * A_33_0, np.power(omega_e, 2))
-    A_55 = A_33_2D * L_c**3/12. + np.divide(U**2 * A_33_0, np.power(omega_e, 2)) - \
-           np.divide(U*L_c**2*B_33_2D, np.power(2*omega_e, 2)) + np.divide(U**2*L_c*A_33_2D, 2*np.power(omega_e, 2))
+    A_55 = A_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * A_33_0, np.power(omega_e, 2))
+    #A_55 = A_33_2D * L_c**3/12. + np.divide(U**2 * A_33_0, np.power(omega_e, 2)) - \
+    #       np.divide(U*L_c**2*B_33_2D, np.power(2*omega_e, 2)) + np.divide(U**2*L_c*A_33_2D, 2*np.power(omega_e, 2))
 
-    #B_55 = B_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * B_33_0, np.power(omega_e, 2))
-    B_55 = B_33_2D*L_c**3/12. + np.divide(U**2 * B_33_0, np.power(omega_e, 2)) + U*L_c**2*A_33_2D/4. \
-           - np.divide(U**2 * L_c * B_33_2D, 2*np.multiply(omega_e, 2))
+    B_55 = B_33_2D * L_c ** 3 / 12. + np.divide(U ** 2 * B_33_0, np.power(omega_e, 2))
+    #B_55 = B_33_2D*L_c**3/12. + np.divide(U**2 * B_33_0, np.power(omega_e, 2)) + U*L_c**2*A_33_2D/4. \
+    #       - np.divide(U**2 * L_c * B_33_2D, 2*np.multiply(omega_e, 2))
 
     # TODO: Make sure if I need to multiply by 2 to include both side hulls
     return 2*A_33, 2*A_35, 2*A_53, 2*A_55, 2*B_33, 2*B_35, 2*B_53, 2*B_55
 
-def compute_hydrostatic_coeff(m, L_oa, L_c, b_s, rho_w=1025., g=9.81):
+def compute_hydrostatic_coeff(m, L_oa, L_c, b_s, kb, zb, zg, rho_w=1025., g=9.81):
     A_w = 2*L_oa*b_s
     C_33 = rho_w * g * A_w
     C_35 = 0.0
     C_53 = 0.0
     C_55 = 0.0  # TODO: Need to implement this correctly
-    #C_55 = m * g * ((1 - kb) * zb - zg) + rho_w * g * (L_c ** 3 * b_s / 6.)
+    C_55 = m * g * ((1 - kb) * zb - zg) + rho_w * g * (L_c ** 3 * b_s / 6.)
 
     return C_33, C_35, C_53, C_55
 
